3cc079b6af30c7ba6a4d1c2637756d2d1256f788
Nana Mohamadi <adilnana78@gmail.com>
2025-08-01 20:55:52 +0000

    Complete Navbar component and guest, about and contact pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Guest from "@/components/Guest"
+import { checkUser } from "@/lib/checkUser"
 
-const Home = () => {
+const Home = async() => {
+  const user = await checkUser()
+  if(!user){
+    return <Guest />
+  }
   return (
     <div>Home</div>
   )
